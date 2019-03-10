ce29184a6536fc0c2ee4491cfdf2578a5f0a83c3
qichanna <qichanna@126.com>
2019-03-10 17:29:47 +0800

    first

---
+++ App/src/main/java/com/kotlin/mail/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.mail.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.mail.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
