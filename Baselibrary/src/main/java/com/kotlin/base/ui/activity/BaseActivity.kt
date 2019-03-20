ba6aead2b3dabb18042c151c2d38a5667470d5c2
liqi7 <liqi7@lenovo.com>
2019-03-20 19:04:02 +0800

    three 1

--- Baselibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
+++ Baselibrary/src/main/java/com/kotlin/base/ui/activity/BaseActivity.kt
@@ -1,6 +1,20 @@
 package com.kotlin.base.ui.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.kotlin.base.common.AppManager
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 
 open class BaseActivity:RxAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        AppManager.instance.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AppManager.instance.finishActivity(this)
+    }
 }
